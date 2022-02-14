020034ee39cbc5ab8a8596ac74a85f1e255aa8ba
paigeevans12 <Paige_Evans@live.co.uk>
2022-02-14 21:58:55 +0000

    added Reccomended Games from RAWG API

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState } from "react";
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GamesList from "./components/GamesList";
 import Header from "./components/Header";
@@ -7,94 +7,94 @@ import Profile from "./components/Profile";
 import Registration from "./components/Registration";
 import About from "./components/About";
 import Search from "./components/Search";
-import axios from "axios";
+import ReccomendedGames from "./components/ReccomendedGames";
+import GameDetail from "./components/GameDetail";
 import "./styles/App.css";
 
+
 function App() {
-  const [userInput, setTerm] = useState("");
 
-
-  
-
-  const search = (userInput) => {
-    axios({
-      url: "https://api.igdb.com/v4/search",
-      method: 'POST',
-      headers: {
-          'Accept': 'application/json',
-          'Client-ID': 'tayr30mes439bymhib3hwd3ge4ggm4',
-          'Authorization': 'Bearer 4afn2jsnamgpgm89xyozipznkrc79p',
-        },     
-        data:'search "' + (userInput) + '"; fields *;'
-     
-    })
-      .then(response => {
-          console.log(response.data);
-      })
-      .catch(err => {
-          console.error(err);
-      });
-  };
- 
   return (
     <>
-    
-    <BrowserRouter>
-          <div className="wrapper">
-            <Routes>
-              <Route
-                exact
-                path="/"
-                element={
-                  <>
-                    <Header />
-                    <Login />
-                    {/* <button onClick={searchGames}>Search</button> */}
-                    <Search
-                  userInput={userInput}
-                  setTerm={setTerm}
-                  search={search}
-                />
-                   </>
-                }
-              />
-              <Route
-                exact
-                path="/profile"
-                element={
-                  <>
-                    <h1>Your Games</h1>
-                    <Header />
-                    <Profile />
-                    <GamesList />
-                    </>
-                }
-              />
-              <Route
-                exact
-                path="/registration"
-                element={
-                  <>
-                    <Header />
-                    <Registration />
-                   </>
-                }
-              />
-              <Route
-                exact
-                path="/about"
-                element={
-                  <>
-                    <Header />
-                    <About />
-                  </>
-                }
-              />
-            </Routes>
-          </div>
-        </BrowserRouter>
-      </>
+
+      <BrowserRouter>
+        <div className="wrapper">
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <>
+                  <Header />
+                  <Login />
+                  <Search component={Search}/>
+                  
+                  <ReccomendedGames />
+                </>
+              }
+            />
+            <Route
+              exact
+              path="/profile"
+              element={
+                <>
+                  <h1>Your Games</h1>
+                  <Header />
+                  <Profile />
+                  
+                <GameDetail/>
+                  <GamesList />
+                </>
+              }
+            />
+            <Route
+              exact
+              path="/registration"
+              element={
+                <>
+                  <Header />
+                  <Registration />
+                </>
+              }
+            />
+            <Route
+              exact
+              path="/about"
+              element={
+                <>
+                  <Header />
+                  <About />
+                </>
+              }
+            />
+            <Route path='/game/:name' component={GameDetail} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
 export default App;
+
+//const [userInput, setTerm] = useState("");
+// const search = (userInput) => {
+  //   axios({
+  //     url: "https://api.igdb.com/v4/search",
+  //     method: 'POST',
+  //     headers: {
+  //       'Accept': 'application/json',
+  //       'Client-ID': 'tayr30mes439bymhib3hwd3ge4ggm4',
+  //       'Authorization': 'Bearer 4afn2jsnamgpgm89xyozipznkrc79p',
+  //     },
+  //     data: 'search "' + (userInput) + '"; fields *;'
+
+  //   })
+  //     .then(response => {
+  //       console.log(response.data);
+  //     })
+  //     .catch(err => {
+  //       console.error(err);
+  //     });
+  // };
+
--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,31 +1,39 @@
-import React from "react";
+import React, { useState } from 'react';
+import Results from './Results';
 
-const Search = (props) => {
-  const onSubmitSearch = (event) => {
-    event.preventDefault();
-    if (event.target.value !== "") {
-      props.search(props.userInput);
-    }
-    if (event.target.value === "") {
-      props.setTerm("");
-    }
-  };
+const Search = () => {
+
+  const [searchTerm, setSearchTerm] = useState("")
+  const [gameResults, setGameResults] = useState([])
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value)
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    let slug = searchTerm.split(' ').join('-').toLowerCase()
+
+    setGameResults([])
+    fetch(`https://rawg.io/api/games?search=${slug}`)
+    .then(resp => resp.json())
+    .then(({results}) => {
+      results === undefined ? alert('no games found') : setGameResults(results)
+    })
+    setSearchTerm("")
+  }
 
   return (
-    <>
-      <form id="searchAPI" onSubmit={onSubmitSearch}>
-        <input
-          type="search"
-          id="search-bar"
-          name="search-bar"
-          aria-label="Search through site content"
-          value={props.userInput}
-          onChange={(event) => props.setTerm(event.target.value)}
-        />
-        <button className="search-button">Search</button>
-      </form>
-    </>
+    <div className="game-search">
+      <h1>Game Search</h1>
+        <form onSubmit={onSubmit}>
+          <input type="text" value={searchTerm} onChange={handleChange}/>
+          <br></br>
+          <input type="submit"/>
+        </form>
+        <Results gameResults={gameResults} />
+    </div>
   );
-};
+}
 
 export default Search;
